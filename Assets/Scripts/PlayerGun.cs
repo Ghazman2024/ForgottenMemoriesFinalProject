07e70ae876b7166eb5df4fa135d1c94c5cd09cfa
Ghazman2024 <twaissighazi@gmail.com>
2024-05-11 14:25:22 +0300

    Working on gun

--- Assets/Scripts/PlayerGun.cs
+++ Assets/Scripts/PlayerGun.cs
@@ -5,15 +5,33 @@ using UnityEngine;
 
 public class PlayerGun : MonoBehaviour
 {
-    public int amoRemaning = 50;
-    public GameObject amoTxt, amoCreats10, amoCreats15, amoCreats20;
+    public int amoRemaning = 20;
+    public GameObject amoTxt, amoCreats5, amoCreats10, amoCreats15;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F)) // Blocking
+        if (Input.GetKeyDown(KeyCode.F)) // Shooting
         {
             amoRemaning--;
         }
         amoTxt.GetComponent<TMP_Text>().text = amoRemaning.ToString();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.gameObject == amoCreats5)
+            {
+                amoRemaning += 5;
+            }
+            else if (other.gameObject == amoCreats10)
+            {
+                amoRemaning += 10;
+            }
+            else if (other.gameObject == amoCreats15)
+            {
+                amoRemaning += 15;
+            }
+        }
     }
 }
